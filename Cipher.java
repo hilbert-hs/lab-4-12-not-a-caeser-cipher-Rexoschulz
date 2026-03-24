e2840d66cf7d6d36dc652df32bf2f804aa4c0c88
Rexoschulz <schulzoliver111208@gmail.com>
2026-03-24 16:11:22 +0000

    dome

--- Cipher.java
+++ Cipher.java
@@ -1,12 +1,14 @@
 class Cipher {
-  private String[][] leterBlock;
+  private String[][] letterBlock;
   private int numRows;
   private int numCols;
 
   // Constructor: parameters include number of rows, number of columns
 
   public Cipher(int r, int c) {
-
+    numRows = r;
+    numCols = c;
+    letterBlock = new String[r][c];
   }
 
   // method fillBlock(): places a String into letterBlock in row major order
@@ -15,7 +17,16 @@ class Cipher {
   // POSTCONDITION: if str.length() > numRows * numCols, trailing characters are ignored
 
   private void fillBlock(String str) {
-
+      for(int i=0; i< letterBlock.length; i++){
+        for(int j=0; j < letterBlock[i].length; j++){
+          if (str.length()> 0){
+            letterBlock[i][j]= str.substring(0,1);
+            str = str.substring(1,str.length());
+          } else{
+            letterBlock[i][j]="A";
+          }
+        }
+      }
   }
 
   // method encryptBlock(): extracts encrypted message (cipher) from letterBlock in column major order
@@ -23,7 +34,13 @@ class Cipher {
   // @return encrypted String from letterBlock
 
   private String encryptBlock() {
-    return "";
+    String str = "";
+    for(int i=0; i< letterBlock[0].length; i++){
+        for(int j=0; j < letterBlock.length; j++){
+            str += letterBlock[j][i];
+        }
+      }
+    return str;
   }
 
   // method encryptMessage(): encrypts a message using methods fillBlock() and encryptBlock()
@@ -32,6 +49,18 @@ class Cipher {
   // if message is an empty String, returns an empty string
 
   public String encryptMessage(String message) {
-    return "";
+    String fin = "";
+    while(message.length()>0){
+      if (message.length()> numRows * numCols){
+        fillBlock(message.substring(0, numRows * numCols));
+        fin += encryptBlock();
+        message = message.substring(numRows * numCols, message.length());
+      } else{
+        fillBlock(message.substring(0, message.length()));
+        fin += encryptBlock();
+        message = "";
+      }
+    }
+    return fin;
   }
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 class main {
   public static void main(String[] args) {
-
+  //c
   // write code here to test your Cipher class.
   // when you're finished, replace your test code with
   // the tests I provided
+  Cipher cipher1 = new Cipher(2, 5);
+  System.out.println(cipher1.encryptMessage("Highschool"));
+
+  System.out.println();
+
+  Cipher cipher2 = new Cipher(3, 4);
+  System.out.println(cipher2.encryptMessage("Highschool"));
+
+  System.out.println();
+
+  Cipher cipher3 = new Cipher(5, 4);
+  System.out.println(cipher3.encryptMessage("Hilbert will win at state robotics."));
+
 
   }
 }
